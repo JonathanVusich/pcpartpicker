d8833d3b410aa46eea7a94d814619619590ae7d5
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2018-11-04 00:20:56 -0500

    Created basic part generator

--- pcpartpicker/scraper.py
+++ pcpartpicker/scraper.py
@@ -1,4 +1,6 @@
-from .errors import UnsupportedRegion
+from .errors import UnsupportedRegion, UnsupportedPart
+import requests
+import json
 
 class Scraper:
 
@@ -6,11 +8,10 @@ class Scraper:
     _regions = ["au", "be", "ca", "de", "es", "fr",
                     "in", "ie", "it", "nz", "uk", "us"]
     _region = "us"
-    _base_url = "http"
+    _base_url = "https://pcpartpicker.com/products/"
 
     def __init__(self, region: str="us"):
         self._set_region(region)
-
 
     @property
     def region(self) -> str:
@@ -21,6 +22,22 @@ class Scraper:
             raise UnsupportedRegion("Region \'{}\' is not supported!".format(region))
         self._region = region
 
-    def _generate_url(self):
+    def _generate_base_url(self):
         if not region == "us":
-            self._base_url = "https://{}.pcpartpicker.com".format(self._region)
+            self._base_url = "https://{}.pcpartpicker.com/products/".format(self._region)
+
+    def _generate_product_url(self, part: str, page_num: int=1) -> str:
+        return "{}{}/#page={}".format(self._base_url, part, page_num)
+
+    def _retrieve_page_num(self, part: str) -> int:
+        page = requests.get(self._generate_product_url(part))
+        parsed_page = json.loads(page.content.decode('utf-8'))
+        return parsed_page["result"]["paging_data"]["page_blocks"][-1]["page"]
+
+    def _yield_part_data(self, part: str):
+        if not part in self._supported_types:
+            raise UnsupportedPart("Part of type \'{}\' is not supported!".format(part))
+        part_num = self._retrieve_page_num(part)
+        for x in range(part_num):
+            page = requests.get(self._generate_product_url(part, x))
+            yield json.loads(page.content.decode('utf-8'))["result"]["html"]
