564f7573032f4adab2db2498db3299d287e6db11
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2018-11-03 23:22:38 -0500

    Added pytests for API and Scraper

--- pcpartpicker/tests/pcpartpicker_test.py
+++ pcpartpicker/tests/pcpartpicker_test.py
@@ -1,15 +1,37 @@
 import pytest
-from pcpartpicker import *
+from pcpartpicker import API
+from pcpartpicker.errors import UnsupportedRegion
 
+# Ensure that API is initialized with the correct data
 def test_api_default_init():
     api = API()
     assert(api.region == "us")
+    assert(api.supported_types == ["cpu"])
+    assert(api.regions == ["au", "be", "ca", "de", "es", "fr",
+                    "in", "ie", "it", "nz", "uk", "us"])
+    assert(api._database == None)
 
+# Ensure that API can be initialized with a different region
 def test_api_region_init():
     api = API('de')
     assert(api.region == 'de')
 
+# Ensure that API throws the correct error if an incorrect region is input
 def test_api_init_exception():
     with pytest.raises(UnsupportedRegion) as excinfo:
         api = API('oc')
-    assert 'Region oc is not supported!' in str(excinfo.value)
+    assert 'Region \'oc\' is not supported!' in str(excinfo.value)
+
+# Check that API.set_region works correctly
+def test_api_set_region():
+    api = API()
+    assert(api.region == 'us')
+    api.set_region('nz')
+    assert(api.region == 'nz')
+
+# Ensure that API.set_region can handle incorrect input
+def test_api_set_region_incorrect_region():
+    with pytest.raises(UnsupportedRegion) as excinfo:
+        api = API()
+        api.set_region('oc')
+    assert 'Region \'oc\' is not supported!' in str(excinfo)
---
+++ pcpartpicker/tests/scraper_test.py
@@ -0,0 +1,25 @@
+import pytest
+from pcpartpicker.scraper import Scraper
+from pcpartpicker.errors import UnsupportedRegion
+
+# Ensure that the region defaults to "us"
+def scraper_default_init_test():
+    scraper = Scraper()
+    assert(scraper.region == "us")
+    assert(scraper._supported_types == ["cpu"])
+    assert(scraper._regions == ["au", "be", "ca", "de", "es", "fr",
+                    "in", "ie", "it", "nz", "uk", "us"])
+    assert(scraper._region == "us")
+    assert(scraper._base_url == "https://pcpartpicker.com")
+
+# Ensure that the scraper default region can be changed
+def scraper_change_region_test():
+    scraper = Scraper()
+    scraper._set_region = "nz"
+    assert(scraper.region == "nz")
+
+def scraper_region_exception_test():
+    with pytest.raises(UnsupportedRegion) as excinfo:
+        scraper = Scraper()
+        scraper._set_region = "oc"
+    assert 'Region \'oc\' is not supported!' in str(excinfo.value)
