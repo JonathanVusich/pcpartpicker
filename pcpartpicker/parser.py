0c078fb6c1ed0971228a18a5eada0b4901e9f42f
Jonathan Vusich <apianist16@gmail.com>
2018-11-05 11:14:22 -0600

    WIP

---
+++ pcpartpicker/parser.py
@@ -0,0 +1,6 @@
+import lxml
+
+
+class Parser:
+
+    async def _cpu(self, raw_data):
--- pcpartpicker/scraper.py
+++ pcpartpicker/scraper.py
@@ -1,4 +1,5 @@
 from .errors import UnsupportedRegion, UnsupportedPart
+from .parser import Parser
 import asyncio
 from itertools import count
 import aiohttp
@@ -18,6 +19,7 @@ class Scraper:
     def __init__(self, region: str="us"):
         self._set_region(region)
         self._generate_base_url()
+        self._parser = Parser()
 
     @property
     def region(self) -> str:
@@ -35,22 +37,27 @@ class Scraper:
     def _generate_product_url(self, part: str, page_num: int=1) -> str:
         return "{}{}/fetch/?page={}".format(self._base_url, part, page_num)
 
-    async def _retrieve_page_data(self, part: str, page_num: int=1) -> str:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(self._generate_product_url(part, page_num)) as page:
-                return await page.text()
-
-    async def _retrieve_url_list(self, part: str):
-        num = json.loads(await self._retrieve_page_data(part))["result"]["paging_data"]["page_blocks"][-1]["page"]
+    async def _retrieve_url_list(self, session: aiohttp.ClientSession, part: str):
+        num = json.loads(await self._retrieve_page_data(session, part))["result"]["paging_data"]["page_blocks"][-1]["page"]
         return [self._generate_product_url(part, x) for x in range(1, num+1)]
 
-    async def _dispatch_data(self, url_list: list):
+    async def _retrieve_page_data(self, session: aiohttp.ClientSession, part: str, page_num: int=1) -> str:
+        page = await session.request('GET', self._generate_product_url(part, page_num))
+        return await page.text()
+
+    async def _
+
+
+    async def _fetch_data(self, url_list: list):
         pool = ProcessPoolExecutor()
-        async with  aiohttp.ClientSession() as session:
+        async with aiohttp.ClientSession() as session:
             coroutines = ()
 
     async def _retrieve_part_data(self, part: str):
         if part not in self._supported_types:
             raise UnsupportedPart("Part of type \'{}\' is not supported!".format(part))
-        result = asyncio.get_event_loop().run_until_complete(self._retrieve_url_list('cpu'))
-        data = asyncio.get_event_loop().run_until_complete(self._dispatch_data(result))
+        async with aiohttp.ClientSession() as session:
+            urls = await self._retrieve_url_list(session, part)
+        return await self._fetch_data(urls)
+
+    async def _retrieve_
