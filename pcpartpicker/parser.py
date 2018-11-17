1e7505a6720c0928bf5e81f0956658a541fe3de8
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2018-11-17 00:37:56 -0600

    Reworked the parser so that dataclasses can be generated dynamically

--- pcpartpicker/parser.py
+++ pcpartpicker/parser.py
@@ -1,55 +1,83 @@
 import lxml.html
-from .parts import *
 import re
 import time
+from itertools import islice
+from .parts import *
 
 class Parser:
 
-    _parsing_rules = {"wired-network-card":6}
-    _part_mappings = {"wired-network-card":EthernetCard}
+    _float_string = r"(?<![a-zA-Z:])[-+]?\d*\.?\d+"
+    _interface_types = ["PCI", "USB"]
+    _net_speeds = ["Mbit/s", "Gbit/s"]
+    _dollar_sign = "$"
+    _clock_speed = ["GHz", "MHz"]
+    _tdp = [" W"]
+
+    def __init__(self):
+        self._part_funcs = {"wired-network-card":[self._interface, self._network_speed, self._price]}
+        self._part_class_mappings = {"wired-network-card":EthernetCard}
 
     async def _parse(self, part: str, raw_html: str):
-
         part_list = []
-        if part in self._parsing_rules:
+        if part in self._part_funcs:
             start = time.perf_counter()
-            for chunk in self._chunk(await self._retrieve_data(raw_html), self._parsing_rules[part]):
-                func = self._get_func(part)
-                part_list.append(await func(chunk))
+            html = lxml.html.fromstring(raw_html)
+            tags = html.xpath('.//*/text()')
+            async for token in Parser._retrieve_data(tags):
+                part_list.append(await self._parse_token(part, token))
             print(time.perf_counter() - start)
         return part_list
 
-    async def _retrieve_data(self, raw_html: str):
-        html = lxml.html.fromstring(raw_html)
-        return html.xpath('.//*/text()')
-
-    def _chunk(self, iterable, chunk_size):
+    @staticmethod
+    async def _retrieve_data(tags: list):
+        tags = [tag for tag in tags if not tag.startswith(" (")]
         start_index = 0
-        end_index = -1
-        for x in range(0, len(iterable), chunk_size):
-            it = iterable[start_index + x:x + chunk_size + end_index]
-            if it[-1] == "Add":
-                it[-1] = "00.00"
-                start_index -= 1
-                end_index -= 1
+        it = None
+        while start_index < len(tags):
+            for y, tag in enumerate(islice(tags, start_index, None)):
+                if tag == "Add":
+                    it = tags[start_index:start_index + y]
+                    start_index += y+1
+                    break
             yield it
 
-    def _get_func(self, part: str):
-        return getattr(Parser, "_{}".format(part.replace("-", "_")))
+    async def _parse_token(self, part: str, data: list):
+        parsed_data = [data[0]]
+        for x, token in enumerate(islice(data, 1, None)):
+            for func in islice(self._part_funcs[part], x, None):
+                result = await func(token)
+                if isinstance(result, tuple):
+                    parsed_data.extend(result)
+                else:
+                    parsed_data.append(result)
+                if result:
+                    break
+        _class = self._part_class_mappings[part]
+        return _class(*parsed_data)
 
-    @staticmethod
-    def _float_string():
-        return r"(?<![a-zA-Z:])[-+]?\d*\.?\d+"
+    async def _network_speed(self, network_speed: str):
+        for speed in self._net_speeds:
+            if speed in network_speed:
+                nums = re.findall(self._float_string, network_speed)
+                if not nums:
+                    return None, None
+                else:
+                    bits = int(nums[0])
+                    if speed == "Mbit/s":
+                        speed = NetworkSpeed.from_Mbits(bits)
+                    else:
+                        speed = NetworkSpeed.from_Gbits(bits)
+                    if len(nums) == 2:
+                        return speed, int(nums[1])
+                    else:
+                        return speed, 1
 
-    @staticmethod
-    async def _wired_network_card(chunk):
-        if "x" in chunk[2]:
-            connection_data = re.findall(Parser._float_string(), chunk[2])
-            num_ports = int(connection_data[1])
-        else:
-            num_ports = 1
-        price = re.findall(Parser._float_string(), chunk[4])[0]
-        price = Decimal(price)
-        return EthernetCard(chunk[0], price, chunk[1], chunk[2], num_ports)
+    async def _interface(self, interface: str):
+        for inter in self._interface_types:
+            if inter in interface:
+                return interface
 
+    async def _price(self, price: str):
+        if self._dollar_sign in price:
+            return Decimal(re.findall(self._float_string, price)[0])
 
