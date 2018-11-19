4753b5fd002d42c5aa43a264569dd8f0f0909f5e
Jonathan Vusich <apianist16@gmail.com>
2018-11-19 11:20:18 -0600

    WIP

--- pcpartpicker/parser.py
+++ pcpartpicker/parser.py
@@ -1,7 +1,7 @@
 import lxml.html
 import re
 import time
-from itertools import islice
+from itertools import islice, chain
 from .parts import *
 
 class Parser:
@@ -11,21 +11,29 @@ class Parser:
     _net_speeds = ["Mbit/s", "Gbit/s"]
     _wireless_protocol_id = "802.11"
     _case_types = ["ATX", "ITX", "HTPC"]
+    _psu_types = ["ATX", "ITX", "SFX", "TFX", "EPS", "BTX"]
+    _psu_modularity = ["No", "Semi", "Full"]
     _dollar_sign = "$"
     _clock_speed = ["GHz", "MHz"]
     _tdp = [" W"]
 
     def __init__(self):
-        self._part_funcs = {"wired-network-card":[self._interface, self._network_speed, self._price],
-                            "wireless-network-card":[self._interface, self._wireless_protocols, self._price],
-                            "case":[self._case_type, self._retrieve_int, self._retrieve_int, self._case_psu, self._price],
+        self._part_funcs = {"wired-network-card":[self._interface, self._network_speed],
+                            "wireless-network-card":[self._interface, self._wireless_protocols],
+                            "case":[self._case_type, self._retrieve_int, self._retrieve_int, self._psu_wattage],
+                            "power-supply":[self._default, self._psu_type, self._default, self._psu_wattage, self._psu_modular]
                             }
+        for func_list in self._part_funcs.values():
+            func_list.append(self._price)
+
+        self._ids = list(chain(self._interface_types, self._net_speeds, self._wireless_protocol_id, self._case_types,
+                          self._psu_types, self._psu_modularity, self._dollar_sign, self._clock_speed, self._tdp))
         self._part_class_mappings = {"wired-network-card":EthernetCard, "wireless-network-card":WirelessCard,
-                                     "case":Case}
+                                     "case":Case, "power-supply":PSU}
 
     async def _parse(self, part: str, raw_html: str):
         part_list = []
-        if part in self._part_funcs:
+        if part == "power-supply":
             start = time.perf_counter()
             html = lxml.html.fromstring(raw_html)
             tags = html.xpath('.//*/text()')
@@ -71,7 +79,7 @@ class Parser:
         uncalled_funcs = [func for func in self._part_funcs[part] if not func in called_funcs]
         if uncalled_funcs:
             for func in uncalled_funcs:
-                parsed_data.append(await func(" "))
+                parsed_data.append(await func(None))
         _class = self._part_class_mappings[part]
         try:
             return _class(*parsed_data)
@@ -116,15 +124,31 @@ class Parser:
         except ValueError:
             return None
 
-    async def _case_psu(self, psu_string: str):
-        if "W" in psu_string:
-            return int(re.findall(self._float_string, psu_string)[0]), True
-        elif psu_string == " ":
+    async def _psu_wattage(self, psu_string: str):
+        if not psu_string:
             return None
-        return None, False
+        elif "W" in psu_string:
+            return int(re.findall(self._float_string, psu_string)[0]), True
+        return None
+
+    async def _psu_type(self, psu_type: str):
+        for psu in self._psu_types:
+            if psu in psu_type:
+                return psu_type, True
+
+    async def _psu_modular(self, psu_modularity: str):
+        for psu in self._psu_modularity:
+            if psu in psu_modularity:
+                return psu_modularity, True
+
+    async def _default(self, default: str):
+        for string in self._ids:
+            if string in default or string == default:
+                return None
+        return default, True
 
     async def _price(self, price: str):
-        if self._dollar_sign in price:
-            return Decimal(re.findall(self._float_string, price)[0]), True
-        elif price == " ":
+        if not price:
             return Decimal("0.00")
+        elif self._dollar_sign in price:
+            return Decimal(re.findall(self._float_string, price)[0]), True
