a15997d03d830478e5a9935feaaad37a57924f3a
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2018-11-21 01:57:18 -0600

    Parser is no longer asynchronous

--- pcpartpicker/parser.py
+++ pcpartpicker/parser.py
@@ -3,6 +3,7 @@ import re
 import time
 from itertools import islice, chain
 from .parts import *
+import cProfile as profile
 
 class Parser:
 
@@ -28,69 +29,58 @@ class Parser:
         self._part_class_mappings = {"wired-network-card":EthernetCard, "wireless-network-card":WirelessCard,
                                      "case":Case, "power-supply":PSU}
 
-    async def _parse(self, part: str, raw_html: str):
+        self._optional_funcs = {self._psu_series}
+
+    def _parse(self, part: str, raw_html: str):
         part_list = []
         if part in self._part_funcs:
-            start = time.perf_counter()
+            profiler = profile.Profile()
+            profiler.enable()
             html = lxml.html.fromstring(raw_html)
             tags = html.xpath('.//*/text()')
-            async for token in Parser._retrieve_data(tags):
-                part_list.append(await self._parse_token(part, token))
-            print(time.perf_counter() - start)
-            print('hi')
+            tags = [
+                    tag
+                    for tag in tags
+                    if not tag.startswith(" (") if not tag.startswith("{")
+                    ]
+            for token in Parser._retrieve_data(tags):
+                part_list.append(self._parse_token(part, token))
+            profiler.disable()
+            profiler.dump_stats(f"{part}.prof")
         return part_list
 
     @staticmethod
-    async def _retrieve_data(tags: list):
-        tags = [
-            tag
-            for tag in tags
-            if not tag.startswith(" (")
-        ]
+    def _retrieve_data(tags: list):
         start_index = 0
-        it = None
         while start_index < len(tags):
+            it = None
             for y, tag in enumerate(islice(tags, start_index, None)):
                 if tag == "Add":
                     it = tags[start_index:start_index + y]
                     start_index += y+1
                     break
+            if not it:
+                return
             yield it
 
-    async def _parse_token(self, part: str, data: list):
+    def _parse_token(self, part: str, data: list):
         parsed_data = [data[0]]
-        called_funcs = []
-        func_index = 0
-        for x, token in enumerate(islice(data, 1, None)):
-            for func in islice(self._part_funcs[part], func_index, None):
-                result = await func(token)
-                called_funcs.append(func)
-                if result:
-                    if result.tuple:
-                        parsed_data.extend(result.value)
-                    else:
-                        parsed_data.append(result.value)
-                    if result.iterate:
-                        break
-                    func_index += 1
-                    continue
-                continue
-            func_index += 1
-
-        # Ensure price is set
-        uncalled_funcs = [
-                         func
-                         for func in self._part_funcs[part]
-                         if not func in called_funcs
-                         ]
-        if uncalled_funcs:
-            for func in uncalled_funcs:
-                result = await func(None)
-                if result:
-                    if result.tuple:
-                        parsed_data.extend(result.value)
-                    else:
-                        parsed_data.append(result.value)
+        start_index = 1
+        for x, func in enumerate(self._part_funcs[part]):
+            if func in self._optional_funcs:
+                result = func(list(islice(data, start_index+x, start_index+x+2)))
+            else:
+                try:
+                    result = func(data[start_index+x])
+                except IndexError:
+                    result = func(None)
+            if result:
+                if result.tuple:
+                    parsed_data.extend(result.value)
+                else:
+                    parsed_data.append(result.value)
+                if not result.iterate:
+                    start_index -= 1
 
         _class = self._part_class_mappings[part]
         try:
@@ -99,7 +89,7 @@ class Parser:
             print('hi')
 
 
-    async def _network_speed(self, network_speed: str):
+    def _network_speed(self, network_speed: list):
         compatible_speeds = [
             speed
             for speed in self._net_speeds
@@ -116,53 +106,58 @@ class Parser:
                 return Result((NetworkSpeed.from_Mbits(number), int(speed[1])), tuple=True)
             elif len(speed) == 1:
                 return Result((NetworkSpeed.from_Gbits(number), 1), tuple=True)
-            return Result(NetworkSpeed.from_Gbits(number), int(speed[1]), tuple=True)
+            return Result((NetworkSpeed.from_Gbits(number), int(speed[1])), tuple=True)
         return Result(None, iterate=False)
 
-    async def _interface(self, interface: str):
+    def _interface(self, interface: str):
         for inter in self._interface_types:
             if inter in interface:
                 return Result(interface)
 
-    async def _wireless_protocols(self, protocols: str):
+    def _wireless_protocols(self, protocols: str):
         if [x for x in self._wireless_protocol_id if x in protocols]:
             return Result(protocols)
 
-    async def _case_type(self, case_type: str):
+    def _case_type(self, case_type: str):
         for case in self._case_types:
             if case in case_type:
                 return Result(case_type)
 
-    async def _retrieve_int(self, string: str):
+    def _retrieve_int(self, string: str):
         try:
             return Result(int(string))
         except ValueError:
             return None
 
-    async def _psu_wattage(self, psu_string: str):
-        if " W" in psu_string and len([x for x in psu_string if x.isnumeric()]) > 2:
+    def _psu_wattage(self, psu_string: str):
+        if not psu_string or "$" in psu_string:
+            return Result(None, iterate=False)
+        if " W" in psu_string and len([x for x in psu_string if x.isnumeric()]) > 1:
             return Result(int(re.findall(self._float_string, psu_string)[0]))
 
-    async def _psu_type(self, psu_type: str):
+    def _psu_type(self, psu_type: str):
         if psu_type in self._psu_types:
             return Result(psu_type)
 
-    async def _psu_modular(self, psu_modularity: str):
+    def _psu_modular(self, psu_modularity: str):
         if psu_modularity in self._psu_modularity:
             return Result(psu_modularity)
 
-    async def _psu_series(self, series: str):
-        psu_type = await self._psu_type(series)
+    def _psu_series(self, series: list):
+        psu_type = self._psu_type(series[0])
         if psu_type:
+            if self._psu_type(series[1]):
+                return Result(series[0])
             return Result(None, iterate=False)
-        return Result(series)
+        return Result(series[0])
 
-    async def _psu_efficiency(self, efficiency: str):
+    def _psu_efficiency(self, efficiency: str):
         if efficiency == "-":
             return Result(None)
-        return Result(efficiency)
+        elif "80+" in efficiency:
+            return Result(efficiency)
 
-    async def _price(self, price: str):
+    def _price(self, price: str):
         if not price:
             return Result(Decimal("0.00"))
         elif [x for x in self._dollar_sign if x in price]:
