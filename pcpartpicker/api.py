5ff2acd94facb9489d1c193cd02705dee19bd1b0
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-02-10 23:37:31 -0600

    Update api.py

--- pcpartpicker/api.py
+++ pcpartpicker/api.py
@@ -33,6 +33,16 @@ class API:
 
         self._handler._set_region(region)
 
+    def set_concurrent_connections(self, connections: int) -> None:
+        """
+        Public function that allows the user to change the number of concurrent connections to open
+        to PCPartpicker. Higher values typically mean shorter scraping times, but also typically means
+        a higher chance of the connection getting timed out. This value should most always be lowered.
+        :param connections:
+        :return:
+        """
+        self._handler._set_concurrent_connections(connections)
+
     def retrieve(self, *args, force_refresh=False):
         """
         Public function that allows the user to make part requests.
