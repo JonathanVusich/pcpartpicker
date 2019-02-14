6d0b83a3b3a85c1fc3f86aa332bc65e75c319a5a
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-02-14 01:15:53 -0600

    Update test_runner.py

--- test_runner.py
+++ test_runner.py
@@ -1,6 +1,4 @@
 import unittest
 
 if __name__ == "__main__":
-    test_loader = unittest.defaultTestLoader.discover(".")
-    runner = unittest.TextTestRunner()
-    runner.run(test_loader)
+    unittest.main()
