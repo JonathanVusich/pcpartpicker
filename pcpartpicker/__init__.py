3d0301dd59305d4126dc1b4c52fa2b78c6edf3c9
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-03-17 16:41:31 -0500

    Bump version

--- pcpartpicker/__init__.py
+++ pcpartpicker/__init__.py
@@ -1,8 +1,7 @@
 from .api import API
-from .errors import UnsupportedRegion
 from .parts import *
 
 __name__ = ["pcpartpicker"]
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __author__ = 'Jonathan Vusich'
 __email__ = 'jonathanvusich@gmail.com'
