22237dcf62c14beb70a6572c81a9bc5b5c1fa50b
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-03-18 07:53:42 -0500

    Added tests for concurrent connections and multithreading

--- tests/test_api.py
+++ tests/test_api.py
@@ -20,6 +20,8 @@ class APITest(unittest.TestCase):
                                                  "in", "ie", "it", "nz", "uk", "us"})
 
         self.assertEqual(api.region, 'us')
+        self.assertEqual(api.concurrent_connections, 25)
+        self.assertTrue(api.multithreading)
 
     # Ensure that API can be initialized with a different region
     def test_api_region_init(self):
@@ -46,9 +48,40 @@ class APITest(unittest.TestCase):
             api.set_region('oc')
         assert 'Region \'oc\' is not supported for this API!' in str(excinfo.exception)
 
+    def test_api_concurrent_connections_kwd(self):
+        api = API(concurrent_connections=100)
+        self.assertEqual(api.concurrent_connections, 100)
+        self.assertEqual(api._handler.concurrent_connections, 100)
+        self.assertEqual(api._handler._scraper._concurrent_connections, 100)
+
     def test_api_modify_concurrent_connections(self):
         api = API()
+        self.assertEqual(api.concurrent_connections, 25)
+        self.assertEqual(api._handler.concurrent_connections, 25)
         self.assertEqual(api._handler._scraper._concurrent_connections, 25)
-        api = API()
         api.set_concurrent_connections(100)
+        self.assertEqual(api.concurrent_connections, 100)
+        self.assertEqual(api._handler.concurrent_connections, 100)
         self.assertEqual(api._handler._scraper._concurrent_connections, 100)
+        api.set_concurrent_connections(50)
+        self.assertEqual(api.concurrent_connections, 50)
+        self.assertEqual(api._handler.concurrent_connections, 50)
+        self.assertEqual(api._handler._scraper._concurrent_connections, 50)
+
+    def test_api_multithreading_kwd(self):
+        api = API(multithreading=False)
+        self.assertFalse(api.multithreading)
+        self.assertFalse(api._handler.multithreading)
+
+    def test_api_modify_multithreading(self):
+        api = API()
+        self.assertTrue(api.multithreading)
+        self.assertTrue(api._handler._multithreading)
+        api.set_multithreading(False)
+        self.assertFalse(api._handler._multithreading)
+        self.assertFalse(api.multithreading)
+        api.set_multithreading(True)
+        self.assertTrue(api.multithreading)
+        self.assertTrue(api._handler._multithreading)
+
+
