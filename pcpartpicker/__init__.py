b0042acfd828f65f743f6a6b1c336e4ced55fd4c
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-04-19 17:43:48 -0500

    Bumped version

--- pcpartpicker/__init__.py
+++ pcpartpicker/__init__.py
@@ -2,6 +2,6 @@ from .api import API
 from .parts import *
 
 __name__ = ["pcpartpicker"]
-__version__ = '1.1.3'
+__version__ = '1.1.4'
 __author__ = 'Jonathan Vusich'
 __email__ = 'jonathanvusich@gmail.com'
