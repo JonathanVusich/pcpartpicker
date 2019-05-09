8840995976195248a1f71004f7983735729c90b9
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-05-09 13:41:32 -0500

    Bumped version for 1.2.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(file_name: str):
 
 setup(
     name="pcpartpicker",
-    version="1.2.1",
+    version="1.2.2",
     author="Jonathan Vusich",
     author_email="jonathanvusich@gmail.com",
     description="A fast, simple API for PCPartPicker.com.",
