54961c35d92053289cacbc5a754c2d424570692a
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-05-28 23:47:46 -0500

    Bumped version

--- pcpartpicker/__init__.py
+++ pcpartpicker/__init__.py
@@ -1,6 +1,6 @@
 from .api import API
 
 __name__ = ["pcpartpicker"]
-__version__ = '1.2.3'
+__version__ = '2.0.0'
 __author__ = 'Jonathan Vusich'
 __email__ = 'jonathanvusich@gmail.com'
--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ def read(file_name: str):
 
 setup(
     name="pcpartpicker",
-    version="1.2.3",
+    version="2.0.0",
     author="Jonathan Vusich",
     author_email="jonathanvusich@gmail.com",
     description="A fast, simple API for PCPartPicker.com.",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     license="GPL",
-    keywords="pcpartpicker api webscraper",
+    keywords="pcpartpicker api webscraper python3",
     url="https://github.com/JonathanVusich/pcpartpicker",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "utils"]),
     install_requires=read("requirements.txt"),
