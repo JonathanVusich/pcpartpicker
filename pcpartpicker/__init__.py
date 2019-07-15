c5cd3e19220b94e94dc3eceb5d284fc734cab23c
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2019-07-15 18:10:02 -0500

    Bumped version

--- pcpartpicker/__init__.py
+++ pcpartpicker/__init__.py
@@ -1,6 +1,6 @@
 from .api import API
 
 __name__ = ["pcpartpicker"]
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 __author__ = 'Jonathan Vusich'
 __email__ = 'jonathanvusich@gmail.com'
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(file_name: str):
 
 setup(
     name="pcpartpicker",
-    version="2.0.0",
+    version="2.0.1",
     author="Jonathan Vusich",
     author_email="jonathanvusich@gmail.com",
     description="A fast, simple API for PCPartPicker.com.",
